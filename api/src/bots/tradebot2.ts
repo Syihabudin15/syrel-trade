8437f0d03ae8d48609aaf9b7390074f04bf8acac
Syihabudin Tsani <syihabudintsani15@gmail.com>
2026-06-09 14:24:30 +0700

    fix error tradebot2

--- api/src/bots/tradebot2.ts
+++ api/src/bots/tradebot2.ts
@@ -1,4 +1,4 @@
-import { TIMEFRAME, TOP_N } from "../libs/config.js";
+import { TIMEFRAME, TIMEFRAME_HIGHER, TOP_N } from "../libs/config.js";
 import { GetCandles, GetTopFutureVolume } from "../libs/exchange.js";
 import type { ITrade } from "../libs/interfaces.js";
 import {
@@ -21,9 +21,10 @@ const Main = async () => {
   for (const symbol of topVolumeData) {
     try {
       const c1 = await GetCandles(symbol.symbol, TIMEFRAME, 200);
+      const c2 = await GetCandles(symbol.symbol, TIMEFRAME_HIGHER, 200);
       if (c1.candles.length < 200) continue;
 
-      const signal = MeanReversionStrategy(symbol.symbol, c1);
+      const signal = MeanReversionStrategy(symbol.symbol, c1, c2);
       if (!signal) continue;
 
       const longs = actives.filter(
